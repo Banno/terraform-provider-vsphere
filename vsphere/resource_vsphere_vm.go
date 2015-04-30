35ba0752508e64b57f3b78ef49bb80a48448e86f
Luke Amdor <luke.amdor@gmail.com>
2015-04-30 10:01:20 -0500

    make vsphere client lazy
    as not everyone has vsphere creds as env vars

--- vsphere/resource_vsphere_vm.go
+++ vsphere/resource_vsphere_vm.go
@@ -3,7 +3,6 @@ package vsphere
 import (
 	"fmt"
 	"github.com/hashicorp/terraform/helper/schema"
-	"github.com/vmware/govmomi"
 	"github.com/vmware/govmomi/find"
 	"github.com/vmware/govmomi/object"
 	"github.com/vmware/govmomi/vim25/mo"
@@ -52,7 +51,10 @@ func resourceVsphereVM() *schema.Resource {
 }
 
 func resourceVsphereVMCreate(d *schema.ResourceData, meta interface{}) error {
-	client := meta.(*govmomi.Client)
+	client, err := meta.(*Config).Client()
+	if err != nil {
+		return err
+	}
 	if client == nil {
 		return fmt.Errorf("client is nil")
 	}
@@ -139,7 +141,10 @@ func resourceVsphereVMCreate(d *schema.ResourceData, meta interface{}) error {
 }
 
 func resourceVsphereVMRead(d *schema.ResourceData, meta interface{}) error {
-	client := meta.(*govmomi.Client)
+	client, err := meta.(*Config).Client()
+	if err != nil {
+		return err
+	}
 
 	finder := find.NewFinder(client.Client, false)
 
@@ -187,7 +192,10 @@ func resourceVsphereVMRead(d *schema.ResourceData, meta interface{}) error {
 }
 
 func resourceVsphereVMUpdate(d *schema.ResourceData, meta interface{}) error {
-	client := meta.(*govmomi.Client)
+	client, err := meta.(*Config).Client()
+	if err != nil {
+		return err
+	}
 
 	finder := find.NewFinder(client.Client, false)
 
@@ -226,7 +234,10 @@ func resourceVsphereVMUpdate(d *schema.ResourceData, meta interface{}) error {
 }
 
 func resourceVsphereVMDelete(d *schema.ResourceData, meta interface{}) error {
-	client := meta.(*govmomi.Client)
+	client, err := meta.(*Config).Client()
+	if err != nil {
+		return err
+	}
 
 	finder := find.NewFinder(client.Client, false)
 
